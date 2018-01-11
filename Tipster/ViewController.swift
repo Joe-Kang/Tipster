7612a2be0d0e8fe9a05b8809eb1e78cc82d24954
Jihun Kang <j_kang9@u.pacific.edu>
2018-01-11 12:11:18 -0800

    Finished Project

--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -23,51 +23,78 @@ class ViewController: UIViewController {
     @IBOutlet weak var GroupSizeNumber: UISlider!
     @IBOutlet weak var tipPercentageValue: UISlider!
     
-    var subtotal = ""
-    var lowTipValue = 0.05
-    var midTipValue = 0.10
-    var highTipValue = 0.15
+    var subtotal = "0"
+    var lowTipValue: Float = 0.05
+    var midTipValue: Float = 0.10
+    var highTipValue: Float = 0.15
+    var groupSize: Float = 1.0
+    
+    func checkSubtotal(x: String) {
+        if subtotal == "0" {
+            subtotal = x
+        } else {
+            subtotal += x
+        }
+    }
+    
+    func checkDecimal() {
+        for i in subtotal {
+            if i == "." {
+                
+            }
+        }
+    }
     
     @IBAction func oneButton(_ sender: UIButton) {
-        subtotal += "1"
+        checkSubtotal(x:"1")
         subtotalLabel.text = subtotal
+        updateUI()
     }
     @IBAction func twoButton(_ sender: UIButton) {
-        subtotal += "2"
+        checkSubtotal(x:"2")
         subtotalLabel.text = subtotal
+        updateUI()
     }
     @IBAction func threeButton(_ sender: UIButton) {
-        subtotal += "3"
+        checkSubtotal(x:"3")
         subtotalLabel.text = subtotal
+        updateUI()
     }
     @IBAction func fourButton(_ sender: UIButton) {
-        subtotal += "4"
+        checkSubtotal(x:"4")
         subtotalLabel.text = subtotal
+        updateUI()
     }
     @IBAction func fiveButton(_ sender: UIButton) {
-        subtotal += "5"
+        checkSubtotal(x:"5")
         subtotalLabel.text = subtotal
+        updateUI()
     }
     @IBAction func sixButton(_ sender: UIButton) {
-        subtotal += "6"
+        checkSubtotal(x:"6")
         subtotalLabel.text = subtotal
+        updateUI()
     }
     @IBAction func sevenButton(_ sender: UIButton) {
-        subtotal += "7"
+        checkSubtotal(x:"7")
         subtotalLabel.text = subtotal
+        updateUI()
     }
     @IBAction func eightButton(_ sender: UIButton) {
-        subtotal += "8"
+        checkSubtotal(x:"8")
         subtotalLabel.text = subtotal
+        updateUI()
     }
     @IBAction func nineButton(_ sender: UIButton) {
-        subtotal += "9"
+        checkSubtotal(x:"9")
         subtotalLabel.text = subtotal
+        updateUI()
     }
     @IBAction func zeroButton(_ sender: UIButton) {
         if subtotal != "" {
             subtotal += "0"
             subtotalLabel.text = subtotal
+            updateUI()
         }
     }
     @IBAction func dotButton(_ sender: UIButton) {
@@ -76,32 +103,42 @@ class ViewController: UIViewController {
         }
         subtotal += "."
         subtotalLabel.text = subtotal
+        updateUI()
     }
     @IBAction func clearButton(_ sender: UIButton) {
-        subtotal = ""
+        subtotal = "0"
         subtotalLabel.text = subtotal
+        updateUI()
     }
    
     @IBAction func tipPercentage(_ sender: UISlider) {
-        lowTipValue = (Double(tipPercentageValue.value) - 0.05)*100
-        midTipValue = Double(tipPercentageValue.value)*100
-        highTipValue = (Double(tipPercentageValue.value) + 0.05)*100
-        lowTip.text = String(Int(lowTipValue)) + "%"
-        midTip.text = String(Int(midTipValue)) + "%"
-        highTip.text = String(Int(highTipValue)) + "%"
+        lowTipValue = sender.value - 0.05
+        midTipValue = sender.value
+        highTipValue = sender.value + 0.05
+        updateUI()
     }
     @IBAction func groupSize(_ sender: UISlider) {
-        groupSizeDisplay.text = String(Int(GroupSizeNumber.value))
+        groupSize = round(sender.value)
+        groupSizeDisplay.text = String(Int(groupSize))
+        updateUI()
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        subtotalLabel.text = subtotal
+        updateUI()
     }
     
     func updateUI() {
-//        var tlt = Double(subtotal) * lowTipValue
+        totalLowTip.text = String(round(lowTipValue * Float(subtotal)! / groupSize * 100) / 100)
+        lowTotal.text = String(round((lowTipValue * Float(subtotal)! + Float(subtotal)!) / groupSize * 100) / 100)
+        totalMidTip.text = String(round(midTipValue * Float(subtotal)! / groupSize * 100) / 100)
+        midTotal.text = String(round((midTipValue * Float(subtotal)! + Float(subtotal)!) / groupSize * 100) / 100)
+        totalHighTip.text = String(round(highTipValue * Float(subtotal)! / groupSize * 100) / 100)
+        highTotal.text = String(round((highTipValue * Float(subtotal)! + Float(subtotal)!) / groupSize * 100) / 100)
+        lowTip.text = String(Int(lowTipValue*100)) + "%"
+        midTip.text = String(Int(midTipValue*100)) + "%"
+        highTip.text = String(Int(highTipValue*100)) + "%"
     }
     
     
